7ee16f3b4636671c95f31f8818408f4bd76e6ee5
ant-kiryushin <110929041+ant-kiryushin@users.noreply.github.com>
2022-08-16 15:52:14 +0500

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,13 +1,37 @@
-console.log("123");
-console.log("Hello World");
-console.log(123);
+// console.log("123");
+//console.log("Hello World");
+// console.log(123);
 
-console.log("hello" + 123)
+// console.log("hello" + 123)
 
-a = 123
-b = "text"
-console.log(a+b);
+// a = 123
+// b = "text"
+// console.log(a+b);
 
-a = 789 
-b = 987
-console.log(a*b);
+// a = 789 
+// b = 987
+// console.log(a*b);
+
+// Начало 2-го занятия
+
+
+
+// let num = 123
+// num = 321
+// console.log(num)
+
+// const happy ="30.01.1990"
+// console.log (happy);
+
+let number = 1.5;
+console.log(number);
+
+console,log(1/0);
+console.log(Infinity);
+console.log("hello" / 123);
+console.log(NaN + 5);
+console.log(NaN ** 0);
+
+let bigi = 12345678901234n;
+
+console.log(bigi);
